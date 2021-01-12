3a34faa0270e37e4b830240651b94f46abffe165
carminoplata <fortekarma@gmail.com>
2021-01-12 19:59:11 +0100

    Completed React integration using App component

--- webpack/components/App.js
+++ webpack/components/App.js
@@ -1,6 +1,8 @@
+import React from 'react';
+
 function App(props){
   console.log("App Hello World!")
-  return <p>{props.message}</p>;   
+  return <> <p>{props.message}</p> </>;   
 }
 
 export default App;
--- webpack/index.js
+++ webpack/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 //import './index.css';
-import App from './App';
+import App from './components/App';
 //import reportWebVitals from './reportWebVitals';
 /* 
 ReactDOM.render(
@@ -12,6 +12,7 @@ ReactDOM.render(
 );
 */
 console.log('Hello World');
+render(<App message="Welcome" />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
